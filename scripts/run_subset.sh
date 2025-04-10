23b83e310ea41d331441a069889c3fc0a2c62994
zoopr <32989402+zoopr@users.noreply.github.com>
2025-04-10 11:51:52 +0200

    Added timestamps and external timeout controls

--- scripts/run_subset.sh
+++ scripts/run_subset.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 APK_POS=$1 
+CUSTOM_TIME=$2
+TIMEOUT=${CUSTOM_TIME:-"2h"}
+
 for APK in $(cat subset.txt) ; 
 do 
-echo "Working on $APK";
+echo "$(date +%s): Working on $APK";
 tar -xvf $APK_POS $APK; 
-mv $APK /tmp/dreach/sample.apk;  
-sg docker -c "DroidReach/docker/start_batch.sh" > ~/log_$APK 2>&1;
+mv $APK /tmp/dreach/sample.apk; 
+echo "$(date +%s): starting docker batch script"; 
+sg docker -c "timeout $TIMEOUT DroidReach/docker/start_batch.sh" > ~/log_$APK 2>&1;
+echo "$(date +%s): Completed analysis of $APK";
 done
